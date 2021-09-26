6d753940aec61967068fac78253b89727d842f6b
UmerJahangir786 <88608659+UmerJahangir786@users.noreply.github.com>
2021-09-26 07:52:04 -0700

    Project Upload

---
+++ Zuul/src/test/java/ass3/mygame2/TestCase4.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass3.mygame2;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Yamini and Umer Jahangir
+ */
+public class TestCase4 {
+
+    private static String getRoomCreation() {
+    throw new UnsupportedOperationException("Not supported yet."); 
+//To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public TestCase4() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+     }
+    
+    @AfterEach
+    public void tearDown() {
+       }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void testPlayerItem() {
+     String expected = "Kitchen";
+     String actual=TestCase4.getRoomCreation();
+     assertEquals(expected,actual);
+          }
+}
